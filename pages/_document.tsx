d95daa0969dbc13ba4d00dad51980b2610188e5e
Even Østerud <evenost@ulrik.uio.no>
2026-01-15 02:05:27 +0100

    Update to favicon and background pathing (fingers crossed)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,14 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+const isProd = process.env.NODE_ENV === "production";
+const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "dagen-web";
+const basePath = isProd ? `/${repo}` : "";
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/web-design/dagen-logo/dagen_at.svg" />
+        <link rel="icon" href={`${basePath}/web-design/dagen-logo/dagen_at.svg`} />
       </Head>
       <body className="antialiased">
         <Main />
