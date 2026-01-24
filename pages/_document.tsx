481db4579c1fd2709956ed44093a99909f4fc370
Even Østerud <evenost@ulrik.uio.no>
2026-01-24 21:21:09 +0100

    Fix tab icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-const isProd = process.env.NODE_ENV === "production";
-const repo = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "dagen-web";
-const basePath = isProd ? `/${repo}` : "";
-
 export default function Document() {
   return (
     <Html lang="nb">
       <Head>
-        <link rel="icon" href={`${basePath}/web-design/dagen-logo/dagen_at.svg`} />
+        <link
+          rel="icon"
+          type="image/svg+xml"
+          href={`/web-design/dagen-logo/dagen_at.svg?v=2`}
+        />
       </Head>
-      <body style={{ ["--base-path" as any]: basePath }} className="antialiased">
+
+      <body className="antialiased">
         <Main />
         <NextScript />
       </body>
